d5c2e31ba8c4ec7a3e6996e35ebd5eddb54e4ded
Nomentsafidy <mnomentsafidy@gmail.com>
2021-01-31 17:08:39 +0300

    feat education: set content

--- src/components/education/Education.vue
+++ src/components/education/Education.vue
@@ -1,6 +1,48 @@
 <template>
   <div id="m_education">
-      Education work!!
+      <div class="m_title" >Education</div>
+      <div class="m_education_container" >
+        <div class="m_education_item">
+          <div class="m_period" >2005-2007</div>
+          <div class="m_cursus" >Etude Supérieur</div>
+          <div class="m_school" >ENI Fianarantsoa</div>
+          <p class="m_description" >
+            Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+        </div>
+        <div class="m_education_item">
+          <div class="m_period" >2005-2007</div>
+          <div class="m_cursus" >Etude Supérieur</div>
+          <div class="m_school" >ENI Fianarantsoa</div>
+          <p class="m_description" >
+            Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+        </div>
+        <div class="m_education_item">
+          <div class="m_period" >2005-2007</div>
+          <div class="m_cursus" >Etude Supérieur</div>
+          <div class="m_school" >ENI Fianarantsoa</div>
+          <p class="m_description" >
+            Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+        </div>
+        <div class="m_education_item">
+          <div class="m_period" >2005-2007</div>
+          <div class="m_cursus" >Etude Supérieur</div>
+          <div class="m_school" >ENI Fianarantsoa</div>
+          <p class="m_description" >
+            Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+        </div>
+        <div class="m_education_item">
+          <div class="m_period" >2005-2007</div>
+          <div class="m_cursus" >Etude Supérieur</div>
+          <div class="m_school" >ENI Fianarantsoa</div>
+          <p class="m_description" >
+            Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+        </div>
+      </div>
   </div>
 </template>
 
