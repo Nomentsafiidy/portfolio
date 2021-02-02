41aceff1c386b6c599e650478300f8bef7b4d709
Nomentsafidy <mnomentsafidy@gmail.com>
2021-02-02 12:48:12 +0300

    feat header: reponsive

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,24 +1,28 @@
 <template>
-    <div id="m_header" >
-      <div class="m_logo">
-        <a href="#">
-          <span>Maizina.</span>
-        </a>        
-      </div>
-      <nav class="m_navigation">
-        <a href="#aboutMe" @click.prevent="scrollTo('m_about')" >About Me</a>
-        <a href="#skil" @click.prevent="scrollTo('m_skill')" >Skill</a>
-        <a href="#services" @click.prevent="scrollTo('m_services')" >Services</a>
-        <a href="#portofolio" @click.prevent="scrollTo('m_portofolio')" >Portofolio</a>
-        <a href="#education" @click.prevent="scrollTo('m_education')" >Education</a>
-        <a href="#contact" @click.prevent="scrollTo('m_contact')" >Contact</a>
-      </nav>
+  <div id="m_header">
+    <div class="m_logo">
+      <a href="#">
+        <span>Maizina.</span>
+      </a>
     </div>
+    <nav class="m_navigation">
+      <a href="#aboutMe" @click.prevent="scrollTo('m_about')">About Me</a>
+      <a href="#skil" @click.prevent="scrollTo('m_skill')">Skill</a>
+      <a href="#services" @click.prevent="scrollTo('m_services')">Services</a>
+      <a href="#portofolio" @click.prevent="scrollTo('m_portofolio')"
+        >Portofolio</a
+      >
+      <a href="#education" @click.prevent="scrollTo('m_education')"
+        >Education</a
+      >
+      <a href="#contact" @click.prevent="scrollTo('m_contact')">Contact</a>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Header',
+  name: "Header",
   methods: {
     scrollTo(elementId) {
       let el = document.querySelector(`#${elementId}`);
@@ -27,9 +31,9 @@ export default {
         block: "start",
       });
     },
-  }
-}
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style src="./header.scss" lang="scss" ></style>
+<style src="./header.scss" lang="scss"></style>
