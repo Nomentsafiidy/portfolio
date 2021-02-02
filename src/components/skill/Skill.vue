e752117da2b32ca6d99d57690e5fb1aba9f1e624
Nomentsafidy <mnomentsafidy@gmail.com>
2021-02-02 20:46:17 +0300

    feat Skill: set skill content

--- src/components/skill/Skill.vue
+++ src/components/skill/Skill.vue
@@ -4,40 +4,40 @@
       MY SKILL
     </div>
     <div class="m_skill_content">
-      <div class="m_skill_container" >
-        <div class="m_skill_title" >Back End</div>
+      <div class="m_skill_container">
+        <div class="m_skill_title">Back End</div>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Loren Ipsum has been the industry's standard dummy text
+          Various server-side programming languages and frameworks
         </p>
         <div class="m_skill_items">
-          <SkillItem :progress="80*1" title="Node Js" />
-          <SkillItem :progress="75*1" title="Sails Js" />
-          <SkillItem :progress="70*1" title="PHP" />
-          <SkillItem :progress="65*1" title="Symfony" />
+          <SkillItem :progress="80 * 1" title="Node Js" />
+          <SkillItem :progress="75 * 1" title="Sails Js" />
+          <SkillItem :progress="70 * 1" title="PHP" />
+          <SkillItem :progress="65 * 1" title="Symfony" />
         </div>
       </div>
-      <div class="m_skill_container" >
-        <div class="m_skill_title" >Front End</div>
+      <div class="m_skill_container">
+        <div class="m_skill_title">Front End</div>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Loren Ipsum has been the industry's standard dummy text
+          Various client-side programming languages and frameworks
         </p>
         <div class="m_skill_items">
-          <SkillItem :progress="75*1" title="Vue Js" />
-          <SkillItem :progress="80*1" title="Angular" />
-          <SkillItem :progress="85*1" title="Quasar" />
-          <SkillItem :progress="80*1" title="Ionic" />
+          <SkillItem :progress="75 * 1" title="Vue Js" />
+          <SkillItem :progress="80 * 1" title="Angular" />
+          <SkillItem :progress="85 * 1" title="Quasar" />
+          <SkillItem :progress="80 * 1" title="Ionic" />
         </div>
       </div>
-      <div class="m_skill_container" >
-        <div class="m_skill_title" >Web Design</div>
+      <div class="m_skill_container">
+        <div class="m_skill_title">Web Design</div>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Loren Ipsum has been the industry's standard dummy text
+          Software for creating models, retouching and creating images
         </p>
         <div class="m_skill_items">
-          <SkillItem :progress="85*1" title="Creative Design" />
-          <SkillItem :progress="80*1" title="Figma" />
-          <SkillItem :progress="90*1" title="Illustrator" />
-          <SkillItem :progress="75*1" title="Photoshop" />
+          <SkillItem :progress="85 * 1" title="Creative Design" />
+          <SkillItem :progress="80 * 1" title="Figma" />
+          <SkillItem :progress="90 * 1" title="Illustrator" />
+          <SkillItem :progress="75 * 1" title="Photoshop" />
         </div>
       </div>
     </div>
@@ -47,11 +47,11 @@
 <script>
 import SkillItem from "./components/Skillitem";
 export default {
-  name: 'Skill',
+  name: "Skill",
   components: {
-    SkillItem
-  }
-}
+    SkillItem,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
